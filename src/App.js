dc4601856bbcbe8aca1e7f2056697c4b8e96ee24
Shraddha Singh <91796853+Shraddhasingh171@users.noreply.github.com>
2023-05-14 11:52:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState } from 'react';
+ function App() {
+  const [fileContent, setFileContent] = useState(null);
+  const [histogramData, setHistogramData] = useState(null);
+   const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('https://www.terriblytinytales.com/test.txt');
+    const content = await response.text();
+    setFileContent(content);
+    const words = content.toLowerCase().match(/\w+/g);
+    const frequencyMap = {};
+    words.forEach((word) => {
+      frequencyMap[word] = (frequencyMap[word] || 0) + 1;
+    });
+    const sortedData = Object.entries(frequencyMap)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 20)
+      .map((item) => ({ word: item[0], frequency: item[1] }));
+    setHistogramData(sortedData);
+  };
+   const handleExportButtonClick = () => {
+    const csvContent = `data:text/csv;charset=utf-8,${histogramData
+      .map((item) => `${item.word},${item.frequency}`)
+      .join('\n')}`;
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'histogram-data.csv');
+    document.body.appendChild(link);
+    link.click();
+  };
+   return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+      {histogramData && (
+        <div>
+          <h2>Top 20 words by frequency</h2>
+          <ul>
+            {histogramData.map((item) => (
+              <li key={item.word}>{`${item.word}: ${item.frequency}`}</li>
+            ))}
+          </ul>
+          <button type="button" onClick={handleExportButtonClick}>
+            Export
+          </button>
+          <h2>Histogram</h2>
+          {/* Render the histogram using a charting library */}
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
+ export default App;
